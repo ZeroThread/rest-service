5320eed90354c359d69b6773f8711e1675d76221
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2022-01-15 23:21:20 +0100

    Chore ⚙: connect application to mongodb.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import config from 'config';
+import connect from './utils/connect';
+import logger from './utils/logger';
+import routes from './routes';
+
+dotenv.config();
 
 const port = config.get<number>('port');
-dotenv.config();
 const app = express();
 
-app.listen(port, () => {
-  console.log(`🚀 is running on port ${port}`);
+app.listen(port, async () => {
+  logger.info(`App 🚀 is running on port ${port}`);
+  routes(app);
+  await connect();
 });
