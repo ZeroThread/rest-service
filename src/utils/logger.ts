486b4647be5eba52db9d7988d0fb9f9272b70831
Divinewill ZERO <zerothread@users.noreply.github.com>
2022-01-31 14:58:41 +0100

    [ Chore ⚙ ] Implemented custom logger.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,5 @@
 import logger from 'pino';
-import dayjs from 'dayjs';
 import pretty from 'pino-pretty';
-
-// const log = logger({
-//   prettyPrint: true,
-//   base: { pid: false },
-//   timestamp: () => `'time': '${dayjs().format()}'`,
-// });
 
 const stream = pretty({
   colorize: true,
@@ -15,27 +8,3 @@ const stream = pretty({
 const log = logger(stream);
 
 export default log;
-
-// import logger from 'pino';
-// import dayjs from 'dayjs';
-
-// const stream = pretty({
-//   colorize: true,
-// });
-// const log = logger(
-//   { timestamp: () => `'time': '${dayjs().format()}'` },
-//   stream
-// );
-
-// // const log = logger({
-// //   transport: {
-// //     target: `pretty`,
-// //     options: {
-// //       colorize: true,
-// //     },
-// //   },
-// //   base: { pid: false },
-// //   timestamp: () => `'time': '${dayjs().format()}'`,
-// // });
-
-// export default log;
